97a8dfefe3bc4af164e0d2559f99f9f37ed779da
webert maciel <webert_maciel@hotmail.com>
2021-11-13 23:18:25 -0300

    first commit 2master

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,9 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro(nome:" O Problema dos 3", numPaginas: 300);
+        System.out.println(livro1);
+
 
         /*int a = 5;
         int b = 5;
@@ -14,4 +17,38 @@ public class PrimeiroPrograma {
 
 
     }
+    class Livro {
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+    }
 }
+
