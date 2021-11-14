14e215c9c080b17122f8a16ee013131b9a8f7d4e
webert maciel <webert_maciel@hotmail.com>
2021-11-13 21:40:06 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 5;
+        System.out.println("Hello Word  " + (a+b));*/
+
+
+    }
+}
